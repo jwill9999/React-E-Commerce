8ecc69d3d6af6cc831ccb07ba7eac921f31868dc
Jason Williams <jasonwilliams2308@gmail.com>
2019-07-05 17:34:28 +0100

    components formed, styles, hover effect

--- src/components/homepage/homePage.js
+++ src/components/homepage/homePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Directory from '../Directory/Directory';  
 import "./homepage.scss";
 
 function homePage()
@@ -6,38 +7,7 @@ function homePage()
     return (
 
         <div className="homepage">
-            <div className="directory-menu">
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">HATS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">JACKETS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">SNEAKERS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">WOMENS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">MENS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
+          <Directory />
         </div>
     )
 }
